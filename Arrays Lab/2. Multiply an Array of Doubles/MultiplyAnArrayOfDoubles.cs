efae48171bae5ef8ff54e091b3ea623e2d72b44a
petertodorov <petodorov@gmail.com>
2017-06-11 16:52:18 +0300

    Lab 3

--- Arrays Lab/2. Multiply an Array of Doubles/MultiplyAnArrayOfDoubles.cs
+++ Arrays Lab/2. Multiply an Array of Doubles/MultiplyAnArrayOfDoubles.cs
@@ -8,8 +8,8 @@ class MultiplyAnArrayOfDoubles
 {
     static void Main(string[] args)
     {
+        double[] input = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
         double p = double.Parse(Console.ReadLine());
-        double[] input = new double[] { 1.2, 3.0, 12.3, 4.56 };
         for (int i = 0; i < input.Length; i++)
         {
             input[i] *= p;
---
+++ Arrays Lab/3. Smallest Element in Array/SmallestElementInArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class SmallestElementInArray
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int smallest = int.MaxValue;
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i]<smallest)
+            {
+                smallest = input[i];
+            }
+        }
+        Console.WriteLine(smallest);
+    }
+}
+
