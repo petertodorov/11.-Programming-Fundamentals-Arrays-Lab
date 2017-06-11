9964f857309e11e5c119b2c9b292cb8c17773afa
petertodorov <petodorov@gmail.com>
2017-06-11 16:48:31 +0300

    Lab 2

---
+++ Arrays Lab/2. Multiply an Array of Doubles/MultiplyAnArrayOfDoubles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MultiplyAnArrayOfDoubles
+{
+    static void Main(string[] args)
+    {
+        double p = double.Parse(Console.ReadLine());
+        double[] input = new double[] { 1.2, 3.0, 12.3, 4.56 };
+        for (int i = 0; i < input.Length; i++)
+        {
+            input[i] *= p;
+            Console.Write(input[i]+" ");
+        }
+    }
+}
+
