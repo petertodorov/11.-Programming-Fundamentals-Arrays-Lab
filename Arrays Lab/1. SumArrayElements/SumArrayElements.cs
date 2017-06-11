50e4c7a17d8ee019020b3efcb6651484576ed69b
petertodorov <petodorov@gmail.com>
2017-06-11 16:41:04 +0300

    Lab 1

---
+++ Arrays Lab/1. SumArrayElements/SumArrayElements.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class SumArrayElements
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int sum = 0;
+        int[] input = new int[n];
+        for (int i = 0; i < input.Length; i++)
+        {
+            input[i] = int.Parse(Console.ReadLine());
+        }
+        sum = input.Sum();
+        Console.WriteLine(sum);
+    }
+}
+
+
+
+
