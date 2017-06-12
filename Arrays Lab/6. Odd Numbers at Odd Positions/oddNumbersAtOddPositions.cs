0e944507e1181df3b0445b04549d5a644491e291
Peter Todorov <petodorov@gmail.com>
2017-06-12 14:58:20 +0300

    Lab 6

---
+++ Arrays Lab/6. Odd Numbers at Odd Positions/oddNumbersAtOddPositions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class oddNumbersAtOddPositions
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+ 
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (i%2!=0)
+            {
+                if (input[i] % 2 != 0)
+                {
+                    Console.WriteLine($"{i} -> {input[i]}");
+                }
+            }
+        }
+    }
+}
+
