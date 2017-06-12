61268721e382bf01efdf4d763173effde03eb859
Peter Todorov <petodorov@gmail.com>
2017-06-12 13:26:53 +0300

    Lab 4

---
+++ Arrays Lab/4. Rotate Array of Strings/RotateArrayOfStrings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class RotateArrayOfStrings
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ').ToArray();
+        string[] result = new string[input.Length];
+        string last = input[input.Length - 1];
+        result[0] = last;
+        for (int i = 1; i < input.Length; i++)
+        {
+            result[i] = input[i-1];
+        }
+
+
+
+        Console.WriteLine(String.Join(" ",result));
+    }
+}
+
