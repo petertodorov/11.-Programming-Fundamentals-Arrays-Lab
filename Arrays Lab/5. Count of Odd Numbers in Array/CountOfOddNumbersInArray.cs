9e694fd46c74dfcbba1f212e1f951f3942672608
Peter Todorov <petodorov@gmail.com>
2017-06-12 14:46:09 +0300

    Lab 5

---
+++ Arrays Lab/5. Count of Odd Numbers in Array/CountOfOddNumbersInArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class CountOfOddNumbersInArray
+    {
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int counter = 0;
+        counter = CountOddElements(input, counter);
+        Console.WriteLine(counter);
+    }
+
+    private static int CountOddElements(int[] input, int counter)
+    {
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (Math.Abs(input[i]) % 2 == 1)
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
+
